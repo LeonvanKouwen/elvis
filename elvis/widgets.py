89f430ef50e2255b02eb63314afb95eadd87184d
Leon van Kouwen <lvankouwen@gmail.com>
2020-10-21 12:19:17 +0200

    KPI widget can be formatted by user.

--- elvis/widgets.py
+++ elvis/widgets.py
@@ -2,10 +2,8 @@
 Custom widgets.
 """
 
-
 import panel as pn
 import param
-from tornado.ioloop import PeriodicCallback
 
 
 class KPI(param.Parameterized):
@@ -17,7 +15,7 @@ class KPI(param.Parameterized):
     """
     value = param.Number(default=0.0)
 
-    def __init__(self, title="kpi", value=0.0, units="", trip_levels=None, **kwargs):
+    def __init__(self, title="kpi", value=0.0, units="", trip_levels=None, format='{value:.2}', **kwargs):
         """
         :param title: string
         :param value: float
@@ -28,6 +26,7 @@ class KPI(param.Parameterized):
         self.title = title
         self.units = units
         self.value = value
+        self.format = format
         self.trip_levels = trip_levels
         self.html_pane = pn.pane.HTML(self._html())
 
@@ -43,28 +42,17 @@ class KPI(param.Parameterized):
         trip_class = ""
         if self.trip_levels and not self.trip_levels[0] < self.value < self.trip_levels[1]:
             trip_class = "kpi-trip"
+        value = self.format.format(value=self.value)
         html_string = f""" 
             <div class='kpi kpi-div'>
                 <span class='kpi kpi-title'> {self.title} </span>
                 <div>
-                    <span class='kpi kpi-value {trip_class}'> {self.value:.3e} </span>
+                    <span class='kpi kpi-value {trip_class}'> {value} </span>
                     <span class='kpi kpi-units'> {self.units} </span>    
                 </div>
             </div>
             """
         return html_string
-
-    @classmethod
-    def live(cls, link_obj=None, link_attr=None, update_time=300, **kwargs):
-        # Deprecated (for now). using a @gen.coroutine idiom seems more clear. Not sure yet.
-        self = cls(**kwargs)
-
-        def callback():
-            self.value = getattr(link_obj, link_attr)
-
-        PeriodicCallback(callback, update_time).start()
-        return self
-
 
 
 
